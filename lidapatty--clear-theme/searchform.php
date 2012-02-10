b25c19114f564d333f7010a67763f7f3fa644eab
cablop.net <cablop.net@f6dcd245-0a9b-4647-acb5-6aed4c72cb61>
2012-02-10 20:42:14 +0000

    DESIGN changes for slideshow and search button

--- lidapatty--clear-theme/searchform.php
+++ lidapatty--clear-theme/searchform.php
@@ -1,6 +1,7 @@
 			<form method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 				<div>
 					<input type="text" value="" name="s" class="searchinput" />
-					<input type="submit" class=".invent-button" value="<?php _e('Search');?>" />
+					<input type="submit" value="<?php _e('Search');?>" />
+					<?php /*<button type="submit" class="invent-button"><span><?php _e('Search');?></span></button> */ ?>
 				</div>
 			</form>
